9f863df0ed850bca12caa5df94c46424871234d3
marquesNatan <natanmarqies656@gmail.com>
2022-11-12 12:00:12 -0300

    adicionando .gitignore

--- build/default/production/src/board/display_7_seg/display_7_seg_map.p1.d
+++
@@ -1,4 +0,0 @@
-build/default/production/src/board/display_7_seg/display_7_seg_map.p1:  \
-src/board/display_7_seg/display_7_seg_map.c  \
-src/board/display_7_seg/display_7_seg_map.h  \
-src/board/display_7_seg/../../pic18f4520/gpio/gpio.h 
--- build/default/production/src/main.p1.d
+++
@@ -1,8 +0,0 @@
-build/default/production/src/main.p1:  \
-src/main.c  \
-src/pic18f4520/fuse/fuse.h  \
-src/pic18f4520/timer/timer.h  \
-src/pic18f4520/interrupt/interrupt.h  \
-src/pic18f4520/gpio/gpio.h  \
-src/board/pinout/pinout.h  \
-src/board/display_7_seg/display_7_seg_map.h 
